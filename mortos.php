9a5edb5922e34f7de855168edb0ac62f04aca458
Jessé Willian <jessewillian0@gmail.com>
2023-06-18 14:51:41 -0300

    1.0

--- mortos.php
+++ mortos.php
@@ -1,33 +1,37 @@
-<?php 
-    require_once("./views/layouts/header.php");
-    require_once("./models/DAO/FalecidoDAO.php");
+<?php
+require_once("./views/layouts/header.php");
+require_once("./models/DAO/FalecidoDAO.php");
 
-    $falecidos = new FalecidoDAO();
-    $falecidos = $falecidos->selectAll();
+$falecidos = new FalecidoDAO();
+$falecidos = $falecidos->selectAll();
 ?>
 <div class="container">
-    <h1 class="text-center">Mortos</h1>
-    <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">Nome</th>
-      <th scope="col">Cpf</th>
-      <th scope="col">Data de Nascimento</th>
-    </tr>
-  </thead>
-  <tbody>
-  <?php foreach ($falecidos as $falecido) { ?>
-            <tr>
-            <td scope="row" class="align-middle"><?=$falecido['id'] ?></td>
-            <td scope="row" class="align-middle"><?=$falecido['nome'] ?></td>
-            <td scope="row" class="align-middle"><?=$falecido['cpf'] ?></td>
-            <td scope="row" class="align-middle"><?=$falecido['data_morte'] ?></td>
-            </tr>
-        <?php } ?>
-  </tbody>
-</table>
+  <h1 class="text-center">Mortos</h1>
+  <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Nome</th>
+        <th scope="col">Cpf</th>
+        <th scope="col">Data de Nascimento</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php foreach ($falecidos as $falecido) { ?>
+        <tr>
+          <td scope="row" class="align-middle"><?= $falecido['id'] ?></td>
+          <td scope="row" class="align-middle"><?= $falecido['nome'] ?></td>
+          <td scope="row" class="align-middle"><?= $falecido['cpf'] ?></td>
+          <td scope="row" class="align-middle"><?= $falecido['data_morte'] ?></td>
+          <td class="table-light" style="width:15%">
+            <a href="mortos_show.php?id=<?= $falecido['id'] ?>"><button type="button" class="btn btn-primary">Exibir</button></a>
+            <a href="mortos_destroy.php?id=<?= $falecido['id'] ?>"><button type="button" class="btn btn-danger">Excluir</button></a>
+          </td>
+        </tr>
+      <?php } ?>
+    </tbody>
+  </table>
 </div>
-<?php 
-    require_once("./views/layouts/footer.php");
+<?php
+require_once("./views/layouts/footer.php");
 ?>
